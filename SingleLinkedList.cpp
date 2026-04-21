e3665e545fb812be78e6a608a0af231d37e79c9e
“WapaTzy” <wafarozanu22@gmail.com>
2026-04-21 12:04:46 +0700

    no mhs kosong

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -15,3 +15,13 @@ public:
     LinkedList() {
         START = NULL;
     }
+
+    void addNode() {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+       
