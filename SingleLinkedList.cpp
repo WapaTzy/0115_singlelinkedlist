f5a58501744ad8fd9daa03dcd71a81d82116c56d
“WapaTzy” <wafarozanu22@gmail.com>
2026-04-21 12:00:07 +0700

    memperbaiki node

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node {
+public:
+    long long noMhs;
+    Node *next;
+};
+
+class LinkedList {
+    Node *START;
+
+public:
+    LinkedList() {
+        START = NULL;
+    }
+
+    void addNode() {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
