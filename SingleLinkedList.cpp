9016a0a526e3fbf98b69751b213c044d9d6031eb
“WapaTzy” <wafarozanu22@gmail.com>
2026-04-21 12:02:18 +0700

    memindah blok menu

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -23,3 +23,79 @@ public:
 
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim < START->noMhs) {
+            if (START != NULL && nim == START->noMhs) {
+                cout << "\nNomor Mahasiswa tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim >= current->noMhs) {
+            if (nim == current->noMhs) {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
+    bool listEmpty() {
+        return START == NULL;
+    }
+
+    
+    bool search(int nim, Node *&previous, Node *&current) {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs) {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    bool delNode(int nim) {
+        Node *current, *previous;
+
+       
+        if (search(nim, previous, current) == false)
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
+    void traverse() {
+        if (listEmpty())
+            cout << "\nList Kosong\n";
+        else {
+            cout << "\nData di dalam list adalah: \n";
+            Node *currentNode = START;
+
+            while (currentNode != NULL) {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
+};
+
